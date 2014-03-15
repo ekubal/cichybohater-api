66bcaf8cab48a934e6d773b9d228d18e2cb56fdf
ekubal <kuba@ror4.(none)>
2014-03-15 19:17:58 +0100

    Added permalink to field

---
+++ db/migrate/20140315174409_add_permalink_to_field.rb
@@ -0,0 +1,9 @@
+class AddPermalinkToField < ActiveRecord::Migration
+  def self.up
+    add_column :fields, :permalink, :string
+    add_index :fields, :permalink
+  end
+  def self.down
+    remove_column :fields, :permalink
+  end
+end
