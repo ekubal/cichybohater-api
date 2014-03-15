4e6bca49d725a0f37d1779ef6295a271ca2b2707
ekubal <kuba@ror4.(none)>
2014-03-15 19:59:36 +0100

    Interventions - geo coordinates

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -3,8 +3,9 @@ class InterventionsController < ApplicationController
   def create
     @schema = Schema.find(params[:category_id])
 
-    @intervention = Intervention.new device_id: params[:device_id], phone_number: params[:phone_number]
+    @intervention = Intervention.new device_id: params[:device_id], phone_number: params[:phone_number], location_lat: params[:location].try(:[], :latitude), location_lgt: params[:location].try(:[], :longitude)
     @intervention.dump_params(params[:intervention])
+
 
     if @intervention.save
       render json: { 
